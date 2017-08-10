05c2b0b5fca7957f63ce13529c79bae6725c9c75
Ryan Pate <ryanpate@ryans-mbp.attlocal.net>
2017-08-09 22:12:04 -0500

    Final Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,46 +1,118 @@
-    var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s,","t","u","v","w","x","y","z"]
+    console.log("LOADED");
+
+    var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
     var wins = 0;
     var losses = 0;
-    var guessesLeft = 7;
+    var guessesLeft = 12;
     var guessedLetters = []; 
-
-// I need to understand this better //
-    document.onkeyup = function(event) {
-      userGuess = event.key;
+    var computerGuess = null;
     
+  
+    var updateGuessesLeft = function () {
+      document.querySelector("#guessesLeft").innerHTML = guessesLeft;
+    };
 
-    var computerGuess = letters[Math.floor(Math.random() * letters.length)]
-  }
+    var updateComputerGuess = function () {
+        computerGuess = letters[Math.floor(Math.random() * letters.length)];
+    };
 
-      // userGuess = letters? How do I say "If the user guess is in the letter array?"
-      if (userGuess = "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s,","t","u","v","w","x","y","z") {
-        if (userGuess != guessedLetters) {
-          if (userGuess = computerGuess) {
-          wins++;
-          winupdate();
-          reset();
-        }
-        else (userGuess != computerGuess) {
-          guessesLeft --;
-          updateGuessedLetters ();
-        }  
-    }
-   } ;
-      if (guessesLeft =< 1) {
-        alert("Game Over, play again!")
+    var updateGuessedLettersSoFar = function () {
+       document.querySelector("#guessedLetters").innerHTML = guessedLetters.join (", ");
+
+    };
+
+    var reset = function () {
+      guessesLeft = 12;
+      guessedLetters = [];
+      updateComputerGuess ();
+      updateGuessesLeft  ();
+      updateGuessedLettersSoFar ();
+    };
+
+    updateComputerGuess ();
+    updateGuessesLeft ();
+
+   document.onkeyup = function (event) {
+
+      guessesLeft--;
+    
+      var letter = String.fromCharCode(event.keyCode).toLowerCase();
+    
+      guessedLetters.push (letter);
+    
+      updateGuessesLeft ();
+    
+      updateGuessedLettersSoFar ();
+    
+      if (letter === computerGuess){
+    
+        wins++;
+    
+        document.querySelector("#wins").innerHTML = wins;
+    
         reset ();
-      } ;
+    
+      }
+    
+      if (guessesLeft === 0) {
+    
+        losses++;
+    
+        document.querySelector("#losses").innerHTML = losses;
+    
+        reset ();
     
 
-var winupdate = function() {
-  document.querySelector(#wins).innerHTML = ("Wins: " + wins)
-}
-var reset = function() {
-  computerGuess (); // is this going to reset the computers letter?
-  guessesLeft = 7;
-  guessedLetters = null;
-}
-var updateGuessedLetters = function () { // What is the correct way to write this? How do we add the user guess to the array
-   var guessedLetters = String.fromCharCode(event.onkeyup).toLowerCase();
-}
+      }
+    };
+
+
+
+
+
+
+// // I need to understand this better //
+//     computerGuess = function () {
+//       computerGuess = letters[Math.floor(Math.random() * letters.length)];
+// }
+//       console.log(computerGuess);
+
+//     document.onkeyup = function(event) {
+//       userGuess = event.key;
+    
+
+//       // userGuess = letters? How do I say "If the user guess is in the letter array?"
+//       if (letters.indexOf(userGuess) > -1){
+//         if (userGuess != guessedLetters) {
+//           if (userGuess = computerGuess) {
+//           wins++;
+//           winupdate();
+//           reset();
+//         }
+//         else if(userGuess != computerGuess) {
+//           guessesLeft --;
+//           updateGuessedLetters ();
+//         }
+//     }
+//    }
+//     if (guessesLeft <= 1) {
+//       alert("Game Over, play again!")
+//       reset ();
+//     }
+//   }
+    
+
+// var winupdate = function() {
+//   document.querySelector("#wins").innerHTML = ("Wins: " + wins)
+// }
+// var reset = function() {
+//    // is this going to reset the computers letter?
+//   guessesLeft = 7;
+//   guessedLetters = null;
+// }
+
+// var updateGuessedLetters = function () { // What is the correct way to write this? How do we add the user guess to the array
+//    guessedLetters = String.fromCharCode(event.onkeyup).toLowerCase()
+// }
+//    // Need to push to letters //
